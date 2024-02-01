98ff088a96d6d755db3cc1382a70e54338fb0688
Murilo Poltronieri Batista De Campos <49900491874@>
2024-02-01 16:02:45 -0300

    aula1

---
+++ ex02/main.py
@@ -0,0 +1,26 @@
+import os
+from http.server import SimpleHTTPRequestHandler
+import socketserver
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def list_directory(self, path):
+        try:
+            f = open(os.path.join(path, 'index.html'), 'r')
+            self.send_response(200)
+            self.send_header('Content-type', "text/html")
+            self.end_headers()
+            self.wfile.write(f.read().encode('utf-8'))
+            f.close
+            return None
+        except FileNotFoundError:
+            pass
+
+        return super().list_directory(path)
+    
+endereco_ip = '0.0.0.0'
+porta = 8000
+
+with socketserver.TCPServer((endereco_ip, porta), MyHandler) as httpd:
+    print(f"Servidor Iniciado em {endereco_ip}: {porta}")
+    httpd.serve_forever()
+            
