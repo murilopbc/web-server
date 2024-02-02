2920327f77e07860127aa360ca0178d37f6c239d
Murilo Poltronieri Batista De Campos <49900491874@>
2024-02-02 16:58:31 -0300

    pronto

--- aula2/main.py
+++ aula2/main.py
@@ -1,6 +1,7 @@
 import os
 from http.server import SimpleHTTPRequestHandler
 import socketserver
+from urllib.parse import parse_qs
 
 
 class MyHandler(SimpleHTTPRequestHandler):
@@ -21,7 +22,7 @@ class MyHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/login':
             try:
-                with open(os.path.join(os.getcwd(), '/aula1/ex02/login.html'), 'r') as login_file:
+                with open(os.path.join(os.getcwd(), 'login.html'), 'r') as login_file:
                     content = login_file.read()
                 self.send_response(200)
                 self.send_header('Content-type', "text/html")
@@ -31,6 +32,23 @@ class MyHandler(SimpleHTTPRequestHandler):
                 self.send_error(404, "File not found")
         else:
             super().do_GET()
+
+    def do_POST(self):
+        if self.path == '/enviar_login':
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length).decode('utf-8')
+            form_data = parse_qs(body)
+
+            print("Dados do Formulário: ")
+            print("Email: ", form_data.get('email', ['']) [0])
+            print("Senha ", form_data.get('senha', ['']) [0])
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write("Dados recebidos com sucesso!".encode('utf-8'))
+        else:
+            super(MyHandler, self).do_POST()    
     
 endereco_ip = '0.0.0.0'
 porta = 8000
